a78ca2f5f25757578a7324e38f964dfcd8267d9e
Cormac Relf <web@cormacrelf.net>
2021-09-14 08:48:58 +1000

    use Reportable as a marker trait instead, use ResultExt for .report(sink)

--- examples/tryout.rs
+++ examples/tryout.rs
@@ -1,142 +1,165 @@
+use std::num::ParseIntError;
+
+use miette::Severity;
+use miette::{Diagnostic, GraphicalReportHandler, GraphicalTheme};
 use miette_sink::Reported;
 use thiserror::Error;
-use miette::{Diagnostic, GraphicalTheme, GraphicalReportHandler, SourceSpan};
 
 use miette_sink::DiagnosticSink;
 use miette_sink::VecSink;
-use miette_sink::Reportable;
+use miette_sink::{Reportable, ResultExt};
 
 #[derive(Debug, Error, Diagnostic)]
 enum Warning {
-    #[error("idk, if it isn't zero i'm not sure...")]
+    #[error("not the end of the world, but nonzero values scare me")]
     #[diagnostic(severity = "warning", code(app::non_zero))]
     NonZero,
-    #[error("warning only, for {value}")]
-    #[diagnostic(
-        severity = "warning",
-        code(app::warning_only),
-        help("try doing it better next time?")
-    )]
-    WarningOnly {
-        value: i32,
-        src: &'static str, // for ease
-        #[snippet(src, message("This is the part that broke"))]
-        snip: SourceSpan,
-        #[highlight(snip, label("This bit here, officer"))]
-        bad_bit: SourceSpan,
-    },
+    #[error("the number is not the optimal size")]
+    #[diagnostic(transparent)]
+    IntSize(#[from] IntSize),
 }
 
 #[derive(Debug, Error, Diagnostic)]
-enum Errors {
-    #[error("negative numbers are utterly unacceptable")]
-    #[diagnostic(code(app::negative))]
+enum Error {
+    #[error("negative numbers are not allowed")]
+    #[diagnostic(
+        help("we only accept positive numbers. try something that isn't negative"),
+        code(app::negative)
+    )]
     Negative,
-    #[error("that's not right. idk what's wrong but yeah")]
-    #[diagnostic(code(app::thats_not_right))]
-    ThatsNotRight,
-    #[error("too large: {0}")]
+
+    #[error(transparent)]
+    #[diagnostic(code(app::input::parse), help("input an integer only please"))]
+    ParseError(#[from] ParseIntError),
+
+    #[error(transparent)]
+    #[diagnostic(code(app::input::io))]
+    Io(#[from] std::io::Error),
+}
+
+/// Error is reportable, Warning is not.
+/// This lets us use `.report(sink)?` but not on Warning, where it would mean
+/// we end up returning Err but only having warnings in the sink.
+impl Reportable for Error {}
+
+/// A little shorthand, as a treat
+type Sink<'a> = &'a mut dyn DiagnosticSink;
+
+#[derive(Debug, Error, Diagnostic)]
+enum IntSize {
     #[diagnostic(
         severity = "warning",
-        code(app::too_large),
-        help("try doing it better next time?")
+        code(app::int_size::too_large),
+        help("i don't like numbers > 5")
     )]
+    #[error("{0} is a bit big")]
     TooLarge(i32),
-    #[error(transparent)]
     #[diagnostic(
-        severity = "error",
-        code(app::input::parse),
-        help("input an integer only please")
+        severity = "warning",
+        code(app::int_size::way_too_large),
+        help("i REALLY don't like numbers > 10")
     )]
-    ParseError(<i32 as std::str::FromStr>::Err),
+    #[error("{0} is WAY too big")]
+    WayTooLarge(i32),
+}
+
+/// Returns an error if negative.
+fn check_non_negative(num: i32) -> Result<(), Error> {
+    if num < 0 {
+        Err(Error::Negative)
+    } else {
+        Ok(())
+    }
+}
+
+/// Checks an integer is an appropriate size (this will end up as a warning, not an error)
+fn validate_integer_size(num: i32) -> Result<(), IntSize> {
+    if num > 10 {
+        return Err(IntSize::WayTooLarge(num));
+    } else if num > 5 {
+        return Err(IntSize::TooLarge(num));
+    }
+    Ok(())
+}
+
+fn validate_generally(value: i32, sink: Sink) -> Result<(), Reported<Error>> {
+    // Note that check_non_negative does not need to know about the error handling mechanism.
+    // It just needs to create any Err type that's convertible to an Error.
+    //
+    // With Result::report(sink)?, obviously the ? might return from the function.
+    // If it does, the error is reported to the sink.
+    check_non_negative(value).report(sink)?;
+
+    // ... and if not, we continue, and emit some warnings if relevant.
+    if value != 0 {
+        // you can report errors and warnings manually.
+        sink.report(Warning::NonZero);
+    }
+    if let Err(size_warning) = validate_integer_size(value).map_err(Warning::from) {
+        sink.report(size_warning);
+    }
+
+    Ok(())
+}
+
+fn parser_or_whatever(num: &str, sink: Sink) -> Result<i32, Reported<Error>> {
+    // Reported<Error> up the top means anything that can be Into::into()'d into an Error can
+    // be reported.
+    let value: i32 = num.parse().report(sink)?;
+
+    // Obviously that includes an actual Error instance itself, as it is trivially
+    // convertible.
+    fn does_not_error() -> Result<(), Error> {
+        Ok(())
+    }
+    does_not_error().report(sink)?;
+
+    // // Note that if we do exit via ?, the library can't enforce that you have actually reported
+    // // a diagnostic that's an actual Error to phone home about, not just a warning.
+    // // So you could have a top level thing to check if the sink is empty, and if so, report an
+    // // error like "hmm, no errors found, but unsuccessful nevertheless". Or, you could separate
+    // // your error type from your large warnings + errors type, as we have done with Error vs
+    // // Warning.
+
+    // Functions that have the same `-> Result<_, Reported<D>>` structure can be embedded with
+    // plain ? as usual: Reported<D> is a promise that the error has been reported already. No need
+    // to .report(sink)? it again. If you try, rustc will yell at you; Reported<D> does not
+    // implement From<Anything>, nor can it be reported itself, as it does not implement
+    // Diagnostic. This ensures you do not report already-reported errors.
+    validate_generally(value, sink)?;
+
+    Ok(value)
+}
+
+fn value_reader(sink: Sink) -> Result<String, Reported<Error>> {
+    let mut input = String::new();
+    println!("");
+    println!("=> please enter an integer below, non-negative please");
+    std::io::stdin().read_line(&mut input).report(sink)?;
+    Ok(input)
+}
+
+fn full_program(sink: Sink) -> Result<i32, Reported<Error>> {
+    let input = value_reader(sink)?;
+    let result = parser_or_whatever(input.trim(), sink)?;
+    Ok(result)
 }
 
 fn main() {
-    type Sink<'a> = &'a mut dyn DiagnosticSink;
-
-    fn fallible_operation(num: i32) -> Result<i32, Errors> {
-        if num > 5 {
-            return Err(Errors::TooLarge(num));
-        }
-        Ok(num)
-    }
-
-    fn validate_value_no_fluff(num: i32) -> Result<(), Errors> {
-        if num < 0 {
-            Err(Errors::Negative)
-        } else if num < -10 {
-            Err(Errors::ThatsNotRight)
-        } else {
-            Ok(())
-        }
-    }
-
-    fn validate_generally(num: i32, sink: Sink) -> Result<i32, Reported> {
-        if num != 0 {
-            // you can report errors and warnings manually.
-            sink.report(Warning::NonZero);
-        }
-        // with Result::report(sink)?, you can only use ? once
-        // This might return on its own. If it does, the error is reported.
-        validate_value_no_fluff(num).report(sink)?;
-        // We have only so far reported a warning, so we won't return Err(Reported).
-        // return Err(Reported);
-        Ok(num)
-    }
-
-    fn parser_or_whatever(
-        src: &'static str,
-        num: &str,
-        sink: &mut dyn DiagnosticSink,
-    ) -> Result<i32, Reported> {
-
-        // You get less error conversion for free, unfortunately.
-        let num: i32 = num.parse().map_err(Errors::ParseError).report(sink)?;
-
-        // see, we can report the error if desired
-        // the library can't enforce that you don't have Err(x) where x is actually only a
-        // warning. So you could have a top level thing to check if the sink is empty, and if
-        // so, report an error like "hmm, no errors found, but unsuccessful nevertheless".
-        // Or, you could separate your error type from your large warnings + errors type.
-        let val = fallible_operation(num).report(sink)?;
-
-        // validate_generally(val, sink)?;
-
-        let (second_word, position) = src
-            .split_inclusive(' ')
-            .take(2)
-            .fold(("", 0), |(prev_word, consumed), word| {
-                (word, consumed + prev_word.len())
-            });
-
-        let bad_bit = (position, second_word.trim_end().len()).into();
-
-        if val > 3 {
-            sink.report(Warning::WarningOnly {
-                value: val,
-                src,
-                snip: (0, src.len()).into(),
-                bad_bit,
-            });
-        }
-        Ok(val)
-    }
-
     let graphical = GraphicalReportHandler::new_themed(GraphicalTheme::unicode());
-    let src = "one two three\nfour five six";
     let mut sink = VecSink::new(graphical);
-    let mut input = String::new();
-    match std::io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("{}", input);
+    loop {
+        sink.clear();
+        let result = full_program(&mut sink);
+        println!("the returned result = {:?}", result);
+        println!("{:?}", sink);
+        println!("here are the diagnostics we just printed:");
+        for diag in sink.diagnostics() {
+            println!(
+                "- [{:?}] {:?}",
+                diag.severity().unwrap_or(Severity::Error),
+                diag
+            );
         }
-        Err(error) => println!("error: {}", error),
-    }
-    let result = parser_or_whatever(&src, input.trim(), &mut sink);
-    println!("result = {:?}\ndiagnostics:", result);
-    println!("{:?}", sink);
-    for diag in sink.into_inner() {
-        println!("{}", diag);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //!
 
 use core::fmt;
+use core::marker::PhantomData;
 use miette::Diagnostic;
 use miette::ReportHandler;
 
@@ -15,6 +16,7 @@ impl<'a> dyn DiagnosticSink + 'a {
     }
 }
 
+/// A DiagnosticSink that
 pub struct VecSink {
     inner: Vec<Box<dyn Diagnostic>>,
     printer: Box<dyn ReportHandler>,
@@ -27,8 +29,23 @@ impl VecSink {
             printer: Box::new(printer),
         }
     }
+
+    /// When you don't have a `&mut dyn DiagnosticSink`, you can equivalently use this method.
+    pub fn report(&mut self, diag: impl Diagnostic + 'static)
+    where
+        Self: Sized,
+    {
+        self.report_boxed(Box::new(diag));
+    }
+
+    pub fn clear(&mut self) {
+        self.inner.clear()
+    }
     pub fn into_inner(self) -> Vec<Box<dyn Diagnostic>> {
         self.inner
+    }
+    pub fn diagnostics(&self) -> &[Box<dyn Diagnostic>] {
+        &self.inner
     }
 }
 
@@ -47,40 +64,44 @@ impl fmt::Debug for VecSink {
     }
 }
 
-trait SizedDiagnostic: Diagnostic + Sized + 'static {}
-
-impl<T> SizedDiagnostic for T where T: Diagnostic + Sized + 'static {}
-
-pub trait Reportable: Sized {
-    type DiagType;
+pub trait ResultExt<D>: Sized {
     type ReportedType;
     fn report<'s, S: DiagnosticSink + ?Sized>(self, sink: &'s mut S) -> Self::ReportedType;
 }
 
-impl<T, D: Diagnostic + Sized + 'static> Reportable for Result<T, D> {
-    type DiagType = D;
-    type ReportedType = Result<T, Reported>;
+/// An `Err(E)` where `E` can be converted to the relevant `D: Diagnostic` is reportable.
+impl<T, E: Into<D>, D: Reportable + Sized + 'static> ResultExt<D> for Result<T, E> {
+    type ReportedType = Result<T, Reported<D>>;
     fn report<'s, S: DiagnosticSink + ?Sized>(self, sink: &'s mut S) -> Self::ReportedType {
         match self {
             Ok(x) => Ok(x),
-            Err(d) => {
-                sink.report_boxed(Box::new(d));
-                Err(Reported)
+            Err(e) => {
+                sink.report_boxed(Box::new(e.into()));
+                Err(Reported::new())
             }
         }
     }
 }
+
+/// A marker trait for things that can be contained in a [Reported].
+pub trait Reportable: Diagnostic {}
 
 /// A zero-sized error type representing a diagnostic that has been sent to a DiagnosticSink.
 ///
 /// We specifically do not implement Diagnostic here, because we want `Result<_,
 /// Reported>::report()` to be an error. You can't report Reported.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Reported;
-impl std::error::Error for Reported {}
-impl fmt::Display for Reported {
+pub struct Reported<D: Reportable>(PhantomData<D>);
+
+impl<D: Reportable> Reported<D> {
+    fn new() -> Self {
+        Reported(PhantomData)
+    }
+}
+
+impl<D: Reportable> std::error::Error for Reported<D> {}
+impl<D: Reportable> fmt::Display for Reported<D> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Errors were reported")
     }
 }
-
